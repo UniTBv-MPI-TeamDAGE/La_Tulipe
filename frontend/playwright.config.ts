df0e81b0802da12ccc26fecb390dfc74f6875afd
ErnestGMaxim <ernest.maxim@student.unitbv.ro>
2026-04-18 20:12:14 +0300

    fix(ci): Fixed webkit bug

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -16,14 +16,16 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    ...(!process.env.CI ? [
+      {
+        name: 'firefox',
+        use: { ...devices['Desktop Firefox'] },
+      },
+      {
+        name: 'webkit',
+        use: { ...devices['Desktop Safari'] },
+      },
+    ] : []),
   ],
   ...(!process.env.CI && {
     webServer: {
