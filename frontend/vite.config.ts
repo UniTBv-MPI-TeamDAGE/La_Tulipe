ca9f2e7136d1d5e737f39439ca8129efe9e0d322
ErnestGMaxim <ernest.maxim@student.unitbv.ro>
2026-04-21 21:53:08 +0300

    fix(tests): Change vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     host: '0.0.0.0',
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL || 'http://backend:8000',
+        target: process.env.API_TARGET || 'http://localhost:8000',
         changeOrigin: true,
       }
     }
